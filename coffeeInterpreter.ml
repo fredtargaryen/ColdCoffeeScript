1acadaaee71c10a5ede14a6230010defd227b7f5
Shahaan Hassan <sh15g15@soton.ac.uk>
2017-03-10 12:58:44 +0000

    Fixed some next level compilation issues. I think notepadd++ is giving it issues with indentations

--- coffeeInterpreter.ml
+++ coffeeInterpreter.ml
@@ -1,40 +1,41 @@
 exception LookupError;;
 
 (* Language Types *)
-type CoffeeType = IntType | BoolType | StringType | SetType
+type coffeeType = IntType | BoolType | StringType | SetType
 
 (* Language Grammer *)
-type CoffeeTerm = 
-	| TmInt of int 
+type coffeeTerm = 
+	  TmInt of int 
 	| TmBool of bool 
 	| TmString of string 
-	| TmSet of SetType
+	| TmSet of coffeeTerm
 	| TmVar of string
-	| TmAssign of string * CoffeeTerm 
-	| TmLessThan of CoffeeTerm * CoffeeTerm
-	| TmGreaterThan of CoffeeTerm * CoffeeTerm
-	| TmEqualTo of CoffeeTerm * CoffeeTerm
-	| TmIf of CoffeeTerm * CoffeeTerm * CoffeeTerm
-	| TmWhile of CoffeeTerm * CoffeeTerm
-	| TmPlus of CoffeeTerm * CoffeeTerm
-	| TmMinus of CoffeeTerm * CoffeeTerm
-	| TmMult of CoffeeTerm * CoffeeTerm
-	| TmDiv of CoffeeTerm * CoffeeTerm
-	| TmUnion of CoffeeTerm * CoffeeTerm
-	| TmDifference of CoffeeTerm * CoffeeTerm
-	| TmMemberOf of CoffeeTerm * CoffeeTerm
-	| TmConcat of CoffeeTerm * CoffeeTerm
-	| TmIntersect of CoffeeTerm * CoffeeTerm
+	| TmUnit of unit
+	| TmAssign of string * coffeeTerm 
+	| TmLessThan of coffeeTerm * coffeeTerm
+	| TmGreaterThan of coffeeTerm * coffeeTerm
+	| TmEqualTo of coffeeTerm * coffeeTerm
+	| TmIf of coffeeTerm * coffeeTerm * coffeeTerm
+	| TmWhile of coffeeTerm * coffeeTerm
+	| TmPlus of coffeeTerm * coffeeTerm
+	| TmMinus of coffeeTerm * coffeeTerm
+	| TmMult of coffeeTerm * coffeeTerm
+	| TmDiv of coffeeTerm * coffeeTerm
+	| TmUnion of coffeeTerm * coffeeTerm
+	| TmDifference of coffeeTerm * coffeeTerm
+	| TmMemberOf of coffeeTerm * coffeeTerm
+	| TmConcat of coffeeTerm * coffeeTerm
+	| TmIntersect of coffeeTerm * coffeeTerm
 
 
 (* this function is taken from lab 5. I have reasons to believe that it may be useful *)
 (* Function to look up the type of a string name variable in a type environment *)
-let rec lookup env str = match env with 
-   Env [] -> raise LookupError  
+(*(let rec lookup env str = match env with 
+   )   Env [] -> raise LookupError  
   |Env ((name,thing) :: gs) -> 
         ( 
           match (name = str) with 
             true -> thing
            |false -> lookup (Env (gs)) str 
-	)
+	)*)
 ;;
